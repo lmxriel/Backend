cf9941acca5b3e779ec9e6b9b3faddd60130f6ce
pawfectcareadoption <pawfectcareadoption@gmail.com>
2026-02-14 21:48:34 +0800

    backend

--- routes/ProcessRoutes.js
+++ routes/ProcessRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get("/getAllAppointment", auth, ProcessController.getAllAppointment);
 router.put("/updateReview/:id", auth, ProcessController.updateReview);
 router.post("/adoption", auth, ProcessController.submitAdoptionRequest);
+router.put("/updateProfile", auth, ProcessController.updateUserProfile);
 router.get(
   "/appointments/availability",
   ProcessController.getAppointmentAvailability
